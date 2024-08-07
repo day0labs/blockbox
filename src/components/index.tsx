f495972cda423aafe43009fcf3722024da0df957
jon0010 <jonnahuel78@gmail.com>
2024-08-07 03:06:31 -0300

    add footer

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,4 +1,5 @@
 import Navbar from "./navbar/Navbar";
 import CardFeature from "./cardFeature/index";
+import  Footer  from "./footer/index";
 
-export { Navbar, CardFeature };
+export { Navbar, CardFeature, Footer };
